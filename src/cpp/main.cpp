f5a127c46a228793772fefc1c3606f02152e2900
lastis <neitakkikkebanan@gmail.com>
2014-01-30 12:01:34 +0100

    Don't know what these changes are.

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -8,26 +8,24 @@ using namespace std;
 using namespace CPhys;
 using namespace physical::constants;
 
-int main(int argc, const char *argv[])
-{
+int main(int argc, const char *argv[]) {
 	// NOTES:
 	// Nc cannot be less than 1
 	long	seed 	= 123;
 	double 	b 	= 5.260;
 	int 	Nc 	= 8;
-	int 	cubes 	= (Nc-1)*(Nc-1)*(Nc-1);
-	int 	atomsSide = 2*3*(Nc-1)*(Nc-1);
-	int 	atoms 	= cubes*4 + atomsSide + 3*Nc - 2;
 	double	mass 	= 39.948*amu;
 	double 	sigma 	= sqrt(K_B*100/mass);
 	double 	ran = 0;
 	double 	vel = 0;
+	double	e0	= 1;
+	Matrix 	r = Lattice::getFCC(Nc,b);
+	Matrix 	v = Matrix(r);
+	double	atoms = r.getLength();
 	// Output
 	int	width 		= 15;
 	int 	precision 	= 10;
 
-	Matrix 	r = Lattice::getFCC(Nc,b);
-	Matrix 	v = Matrix(r);
 
 	// Initialize the velocities
 	for (int i = 0; i < atoms; i++) {
@@ -39,6 +37,25 @@ int main(int argc, const char *argv[])
 		v(i,2) = vel;
 	}
 
+	/*
+	// Verlet integration
+	double ir = 0;
+	double ir2 = 0;
+	double ir6 = 0;
+	double ir12 = 0;
+	for (int i = 0; i < atoms; i++) {
+		for (int j = 0; j < atoms; j++) {
+			ir = 1/(r(j,0)+r(j,1)+r(j,2));
+			ir2 = ir*ir;
+			ir6 = ir2*ir2*ir2;
+			ir12 = ir6*ir6;
+			F = 24*(2*ir12-ir6);
+			Fx = F*ir2*x;
+			Fy = F*ir2*y;
+			Fz = F*ir2*z;
+		}
+	}
+	*/
 
 	ofstream outFile("fcc.xyz");
 	outFile << atoms << endl;
