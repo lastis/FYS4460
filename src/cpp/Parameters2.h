3d01ea270ae6976e32406aa85ae743db89bfcd54
lastis <neitakkikkebanan@gmail.com>
2014-02-25 14:37:28 +0100

    Ah yes, I did an important change in the parameters file.

--- src/cpp/Parameters2.h
+++ src/cpp/Parameters2.h
@@ -8,11 +8,11 @@ using namespace physical::unit;
 const double 	sigma 	= 3.405; 	//E-10
 const double 	b 	= 5.720/sigma; 	//E-10
 const double 	mass 	= 39.948*amu;
-const double 	T	= 1;
+const double 	T	= 0;
 const double 	e0	= 119.8*K_B;
 const double 	v0	= sqrt(e0/mass);
 const double 	stdDev 	= sqrt(K_B*T/mass);
-const int	Nc	= 8;
+const int	Nc	= 20;
 
 /////////////////////////////////////////////////
 // 		Simulation Variables
@@ -20,8 +20,10 @@ const int	Nc	= 8;
 
 const double 	dt	= 0.02;
 const double 	finalT  = 200*dt;
-const int 	dumpRate 	= 10;
+const int 	dumpRate 	= 1;
 const double	targetT = 0.851;
 const double	tau	= 20*dt;
+
+const bool	usePores = false;
 const double	poreRadius = 2*nm/(sigma*1e-10);
 const int	poreCnt = 5;
