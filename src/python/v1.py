31a7c1bc744e521a8ecd5b36e104385df329f102
lastis <neitakkikkebanan@gmail.com>
2014-04-28 11:37:36 +0200

    added a function to find the mass of the percolation cluster

--- src/python/v1.py
+++ src/python/v1.py
@@ -1,15 +1,14 @@
 from pylab import*
 from scipy.ndimage import measurements
-from numpy import linalg
 
 nsample = 5
 pc = 0.59275
-p = arange(pc,1.0,0.01)
-nx = len(p)
+p = arange(pc-0.1,1.0,0.01)
 
 lStart = 9
 lEnd = 9
-P = zeros((nx,lEnd-lStart+1))
+P = zeros((len(p),lEnd-lStart+1))
+Pi = zeros((len(p),lEnd-lStart+1))
 
 for lCnt in xrange(lEnd - lStart + 1): 
 	lx = pow(2,lStart + lCnt)
@@ -17,21 +16,24 @@ for lCnt in xrange(lEnd - lStart + 1):
 	ll = lx*ly
 	for sample in xrange(0,nsample):
 		r = rand(lx,ly)
-		for i in xrange(0,nx):
+		for i in xrange(0,len(p)):
 			z = r < p[i]
 			lw, num = measurements.label(z)
 			perc_x = intersect1d(lw[0,:],lw[-1, :])
 			perc_y = intersect1d(lw[:,0],lw[ :,-1])
 			perc_u = union1d(perc_x,perc_y)
-			#if ( i == nx-1 and sample == 0):
-			# This line might be uncesseray
 		    	perc = perc_u[where(perc_u > 0)]
 			if (len(perc) > 0):
 				area = measurements.sum(z,lw,index=perc)
 				totSum = sum(area)
 				P[i,lCnt] += totSum/ll
+				Pi[i,lCnt] += 1
 
+Pi /= nsample
 P /=  nsample
+plot(p,Pi)
+show()
+
 plot(p,P)
 show()
 
@@ -39,5 +41,5 @@ P = log(P)
 p = log(p-pc)
 
 plot(p,P)
-show()
+#show()
 				
